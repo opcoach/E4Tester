4954638331917d4c219c1c8fb882060a78cb1978
Olivier Prouvost <olivier.prouvost@opcoach.com>
2018-01-27 15:16:41 +0100

    Fix #5 : Add a test fragment to provide unit tests
    Empty test for the moment...
    
    Change-Id: I30caffb5c3f067d95d60633801c066bff4716a67
    Signed-off-by: Olivier Prouvost <olivier.prouvost@opcoach.com>

---
+++ com.opcoach.e4tester.core.test/src/com/opcoach/e4tester/core/test/WidgetAccessTest.java
@@ -0,0 +1,21 @@
+package com.opcoach.e4tester.core.test;
+
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class WidgetAccessTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	// JUnit 5 : @DisplayName("Label widget is found")
+	public void test() {
+		assertTrue(true);
+	}
+
+}
