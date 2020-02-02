ae914192f80ac0cf1b445126cd0c40404bf97e0a
lacherp <pascal.lacherez@thalesaleniaspace.com>
2020-02-02 11:33:59 +0100

    rg co

--- com.opcoach.e4tester.test.components/src/com/opcoach/e4tester/test/nattable/components/NattablePart1.java
+++ com.opcoach.e4tester.test.components/src/com/opcoach/e4tester/test/nattable/components/NattablePart1.java
@@ -40,6 +40,7 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
